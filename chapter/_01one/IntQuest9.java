876ec2a5bd4eb89c6ae4210ba65e1abcf4727efc
msSarriman <sarriman@hotmail.com>
2019-05-24 02:05:15 +0300

    master

--- chapter/_01one/IntQuest9.java
+++ chapter/_01one/IntQuest9.java
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright (c) 2019 Emmanouil Sarris
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 /**
  * String Rotation: Assume you have a method isSubstring which checks if _01one word is a substring
  * of another. Given _02two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only _01one
